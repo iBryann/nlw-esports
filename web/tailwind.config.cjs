25f6578c6e1bd5ada7cd2d83ecd504649de12fca
Bryann Henrique <bryann.enrique@gmail.com>
2022-09-14 18:35:50 -0300

    frontend: ajuste no gradiente e no title do site.

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
         backgroundImage: {
             galaxy: 'url(/background-galaxy.png)',
-            'nlw-gradient': 'linear-gradient(90deg, #9572FC, #43E7AD, #E1D55D)',
+            'nlw-gradient': 'linear-gradient(86deg, #9572FC 20.08%, #43E7AD 45.94%, #E1D55D 85.57%)',
             'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67%)'
         }
     },
