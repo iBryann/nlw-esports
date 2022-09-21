d2b04792acee971a1f1af6001062639ead9aa1c4
Bryann Henrique <bryann.enrique@gmail.com>
2022-09-21 15:45:07 -0300

    mobile: listagem de games e anúncios finalizadas.

---
+++ mobile/src/screens/Game/styles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    header: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 32,
+        marginTop: 28,
+        justifyContent: 'space-between'
+    },
+    logo: {
+        width: 72,
+        height: 40
+    },
+    right: {
+        width: 25,
+        height: 25
+    },
+    cover: {
+        width: 311,
+        height: 160,
+        marginTop: 32,
+        borderRadius: 8
+    },
+    containerList: {
+        width: '100%'
+    },
+    contentList: {
+        paddingLeft: 32,
+        paddingRight: 64,
+        alignItems: 'flex-start'
+    },
+    emptyListContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    emptyListText: {
+        textAlign: 'center',
+        color: THEME.COLORS.CAPTION_300,
+        fontSize: THEME.FONT_SIZE.SM,
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+    }
+});
